bd31ae24df60348c222bed5224c41b77467d5670
zlatko.bandrov@abv.bg <zlatko.bandrov@abv.bg>
2017-07-20 21:24:32 +0300

    Initial commit Empty project

---
+++ ZlatkoBandrov.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZlatkoBandrov.WebApp.MvcApplication" Language="C#" %>
---
+++ ZlatkoBandrov.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZlatkoBandrov.WebApp.Startup))]
+namespace ZlatkoBandrov.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
